6efd046e700d2bd047c36e6236ca9fbb19a37703
Abdurrahman Shofy Adianto <azophy@gmail.com>
2023-12-08 15:02:38 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+EXAMPLE HTTP API TO REPLACE TEXT IN PDF FILE
+============================================
+
+Using Python 3 with FastAPI (for HTTP API) and borb (for PDF processing)
+
+## How to install
+1. clone
+1.b. (optional) setup a pip venv `python -m venv .venv ; . ./venv/bin/activate`
+2. `pip install -r requirements.txt`
+
+## How to run
+
+```
+uvicorn server:app --port 8000 --reload
+```
---
+++ replace.py
@@ -0,0 +1,32 @@
+# from https://stackoverflow.com/a/75574195
+from borb.pdf import Document
+from borb.pdf import PDF
+from borb.toolkit import SimpleFindReplace
+
+import typing
+
+def replace_text_in_pdf(input_filename, output_filename, replaced_text, new_text):
+
+    # attempt to read a PDF
+    doc: typing.Optional[Document] = None
+    with open(input_filename, "rb") as pdf_file_handle:
+        doc = PDF.loads(pdf_file_handle)
+
+    # check whether we actually read a PDF
+    assert doc is not None
+
+    # find/replace
+    doc = SimpleFindReplace.sub(replaced_text, new_text, doc)
+
+    # store
+    with open(output_filename, "wb") as pdf_file_handle:
+        PDF.dumps(pdf_file_handle, doc)
+
+
+if __name__ == "__main__":
+    replaced_text(
+        "test.pdf",
+        "result.pdf",
+        "{{nomor_surat}}",
+        "hasil_replace"
+    )
